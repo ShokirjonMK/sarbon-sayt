70127143f06bc1d0a9a5efe3dd47984e6d2aa887
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2024-09-21 11:18:21 +0500

    directions link ozgardi

--- src/components/Cards/directionSliderCardv1.tsx
+++ src/components/Cards/directionSliderCardv1.tsx
@@ -4,12 +4,13 @@ import MainButton from "../Buttons/mainBtn";
 const DirectionSliderCardv1 = ({className, item}: {className?: string, item?: any}) => {
 
     return (
-        <Link href={`/directions/${item?.id}`} className="hover:-translate-y-2 transition-all" >
+        <div className="hover:-translate-y-2 transition-all" >
+        {/* <Link href={`/directions/${item?.id}`} className="hover:-translate-y-2 transition-all" > */}
             <div className={`rounded-lg p-2 main-card-hover cursor-pointer h-full ${className}`} style={{border: 'solid 1px #e9ebeb'}}>
                 <div className="py-3 px-4 flex flex-col h-full">
                     <p className="text-2xl mb-3 card-title">{item?.title}</p>
                     <p className="line-clamp-2 mb-8 ">{item?.sub_title}</p>
-                    <div className="mb-4 mt-auto">
+                    <div className="mb-4 mt-auto text-primary">
                         {
                             item?.prices?.kunduzgi ? 
                             <div className="flex justify-between">
@@ -32,15 +33,15 @@ const DirectionSliderCardv1 = ({className, item}: {className?: string, item?: an
                             </div> : ""
                         }
                     </div>
-                    <MainButton
+                    {/* <MainButton
                         title="Batafsil"
                         isArrow={false}
                         type="main"
                         className="w-full justify-center xl:py-[8px] text-[14px] font-medium"
-                    />
+                    /> */}
                 </div>
             </div>
-        </Link>
+        </div>
     )
 }
 export default DirectionSliderCardv1;
